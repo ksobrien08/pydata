6d0dde2240fe0bd9f3ff8b511be098d7ac9f3df3
ksobrien08 <ksobrien08@gmail.com>
2024-10-21 22:41:50 -0500

    nba methods done

--- nba.py
+++ nba.py
@@ -1,23 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import PlayerDashboardByGeneralSplits
+from nba_api.stats.endpoints import commonplayerinfo
 
-# Send a GET request to the NBA.com player statistics page
-response = requests.get("https://www.nba.com/stats/players")
+"""
+https://pypi.org/project/nba-api/
+https://github.com/datasets/nba/blob/main/docs/nba_api.md
 
-# Parse the HTML content
-soup = BeautifulSoup(response.content, "html.parser")
+# with pd.option_context('display.max_columns', None):
+#     print(player_bio.get_data_frames()[0])
 
-# Extract the player information
-player_stats = []
-for player_row in soup.select("table.stats-table tbody tr"):
-    player_info = {
-        "name": player_row.select_one("td.player-name").text.strip(),
-        "points": player_row.select_one("td.points").text.strip(),
-        "rebounds": player_row.select_one("td.rebounds").text.strip(),
-        "assists": player_row.select_one("td.assists").text.strip(),
-    }
-    player_stats.append(player_info)
+"""
 
-# Process and store the scraped data
-for player in player_stats:
-    print(player)
+
+def get_career_stats(name):
+    player_id = [player['id'] for player in players.get_players() if player['full_name'] == name][0]
+    career_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
+    return career_stats.get_data_frames()[0]
+
+def get_player_stats(name):
+    player_id = [player['id'] for player in players.get_players() if player['full_name'] == name][0]
+    season_stats = PlayerDashboardByGeneralSplits(player_id=player_id, season='2022-23')
+    return season_stats.get_data_frames()[0]
+
+def get_player_bio(name):
+    player_id = [player['id'] for player in players.get_players() if player['full_name'] == name][0]
+    player_bio = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
+    return player_bio.get_data_frames()[0]
+
+def main():
+    """
+    Main function for the NBA stats program.
+    """
+    name = input("Enter player name: ")
+    print(get_career_stats(name))
+    print(get_player_stats(name))
+    print(get_player_bio(name))
+
+    
+if __name__ == "__main__":
+    main()

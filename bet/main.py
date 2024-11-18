f193bdaf731c1454d6a4b5d2edf134af7ef28c38
Kaleb B <kalebbaugh@Baugh-MBP.attlocal.net>
2024-11-17 20:20:55 -0600

    Saving

---
+++ bet/main.py
@@ -0,0 +1,13 @@
+# hello.py
+
+def hello_world():
+    """Prints a hello world message"""
+    print("Hello, World!")
+
+def main():
+    """Main function"""
+    print("Calling hello_world function...")
+    hello_world()
+
+if __name__ == "__main__":
+    main()
